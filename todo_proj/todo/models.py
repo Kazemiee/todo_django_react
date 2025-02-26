e36f1ab0c217606732ee8f8b71f11d05e8e274d6
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2025-02-26 05:47:35 -0500

    model

---
+++ todo_proj/todo/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Task(models.Model):
+    task_id = models.IntegerField()
+    text = models.CharField(max_length=200)
+    difficulty = models.IntegerField()
+
+    def __str__(self):
+        return self.text
+
+
+class TaskOrdering(models.Model):
+    # Deletes reservation if user account is deleted
+    first_task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='first')
+    second_task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='second')
+
+    def __str__(self):
+        return f"{self.first_task} -> {self.second_task}"
